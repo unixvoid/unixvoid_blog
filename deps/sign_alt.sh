e81f6de0d713607f675623672927d9b01b95ab5e
Matthew Faltys <mfaltys@gmail.com>
2017-02-05 19:41:56 -0600

    Debug travis build

--- deps/sign_alt.sh
+++ deps/sign_alt.sh
@@ -1,10 +1,12 @@
- #!/bin/bash
- GIT_HASH=$(shell git rev-parse HEAD | head -c 10)
+#!/bin/bash
+GIT_HASH=$(shell git rev-parse HEAD | head -c 10)
 
- echo $1 | gpg \
-	--passphrase-fd 0 \
-	--batch --yes \
-	--no-default-keyring --armor \
-	--secret-keyring ./unixvoid.sec --keyring ./unixvoid.pub \
-	--output unixvoid_blog-$GIT_HASH-linux-amd64.aci.asc \
-	--detach-sig unixvoid_blog.aci
+echo $1 | gpg \
+       --passphrase-fd 0 \
+       --batch --yes \
+       --no-default-keyring --armor \
+       --secret-keyring ./unixvoid.sec --keyring ./unixvoid.pub \
+       --output unixvoid_blog-$GIT_HASH-linux-amd64.aci.asc \
+       --detach-sig unixvoid_blog.aci
+
+mv unixvoid_blog.aci unixvoid_blog-$GIT_HASH-linux-amd64.aci
